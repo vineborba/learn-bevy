b7ffc4bc24b6399d94cfef28fb5006bd262604f8
Vinicius de Borba <vinicius.borba@softdesign.com.br>
2025-11-05 16:55:49 -0300

    refactor: change movement logic to use explict system ordering

--- src/enemy/mod.rs
+++ src/enemy/mod.rs
@@ -18,9 +18,15 @@ impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut bevy::app::App) {
         app.init_resource::<EnemySpawnTimer>()
             .add_systems(Startup, spawn_enemies)
-            .add_systems(Update, enemy_movement)
-            .add_systems(Update, update_enemy_direction)
-            .add_systems(Update, confine_enemy_movement)
+            .add_systems(
+                Update,
+                (
+                    enemy_movement,
+                    confine_enemy_movement,
+                    update_enemy_direction,
+                )
+                    .chain(),
+            )
             .add_systems(Update, tick_enemy_spawn_timer)
             .add_systems(Update, spawn_enemies_over_time);
     }
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -13,8 +13,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
-            .add_systems(Update, player_movement)
-            .add_systems(Update, confine_player_movement)
+            .add_systems(Update, (player_movement, confine_player_movement).chain())
             .add_systems(Update, enemy_hit_player)
             .add_systems(Update, player_hit_star);
     }
